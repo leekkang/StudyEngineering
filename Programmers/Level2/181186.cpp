72a1269a90a66eb09a288c6a9bb62bc1accf64b7
Leekkang <qkaxhfdl93@gmail.com>
2023-04-21 23:52:39 +0900

    풀이 추가

---
+++ Programmers/Level2/181186.cpp
@@ -0,0 +1,51 @@
+﻿
+#include <iostream>
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/181186?language=cpp
+
+#include <string>
+#include <vector>
+#include "math.h"
+
+using namespace std;
+
+int solution(int n) {
+	int answer = 0;
+	vector<long long> vec(100001, 0);
+	vec[1] = 1;
+	vec[2] = 3;
+	vec[3] = 10;
+	vec[4] = 23;  // 10 + 2 * 3  + 5 * 1  + 2
+	vec[5] = 62;  // 23 + 2 * 10 + 5 * 3  + 2 * 1 + 2 
+	vec[6] = 170; // 62 + 2 * 23 + 5 * 10 + 2 * 3 + 2 * 1 + 4
+
+	// 추가되는 패턴의 개수
+	// 1, 2, 5, 2, 2, 4, 2, 2, 4 ...
+	// f(x)     = f(x - 1) + 2 * f(x - 2) + 5 * f(x - 3) + 2 * f(x - 4) + 2 * f(x - 5) + 4 * f(x - 6) ...
+	// f(x + 3) = f(x + 2) + 2 * f(x + 1) + 5 * f(x) + 2 * f(x - 1) + 2 * f(x - 2) + 4 * f(x - 3) ...
+	// f(x + 3) - f(x) = f(x + 2) + 2 * f(x + 1) + 5 * f(x) + f(x - 1) - f(x - 3)
+
+	// 최종식 : f(x + 3) = f(x + 2) + 2 * f(x + 1) + 6 * f(x) + f(x - 1) - f(x - 3)
+
+	long long moduler = 1000000007;
+	for (int i = 7; i <= n; ++i) {
+		vec[i] = vec[i - 1] + vec[i - 2] * 2 + vec[i - 3] * 6 + vec[i - 4] - vec[i - 6];
+		if (vec[i] > 0)
+			vec[i] %= moduler;
+		else {
+			do {
+				vec[i] += moduler;
+			} while (vec[i] < 0);
+		}
+	}
+	answer = (int)vec[n];
+
+	return answer;
+}
+
+int main() {
+	int result = solution(12345);
+	cout << result << endl;
+
+	return 0;
+}
---
+++ Programmers/Level2/181187.cpp
@@ -0,0 +1,38 @@
+﻿
+#include <iostream>
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/181187?language=cpp
+
+#include <string>
+#include <vector>
+#include "math.h"
+
+using namespace std;
+
+long long solution(int r1, int r2) {
+    long long answer = 0;
+
+	long long square1 = (long long)r1 * r1;
+	long long square2 = (long long)r2 * r2;
+
+	for (int i = 1; i < r2; ++i) {
+		long long squareX = (long long)i * i;
+		if (i < r1) {
+			int boundary1 = (int)ceil(sqrtl(square1 - squareX));
+			int boundary2 = (int)floor(sqrtl(square2 - squareX));
+			answer += (boundary2 - boundary1 + 1);
+		} else {
+			answer += ((int)floor(sqrtl(square2 - squareX)) + 1);
+		}
+	}
+	++answer;	// i == r2
+
+	return answer * 4;
+}
+
+int main() {
+	long long result = solution(1, 1000000);
+	cout << result << endl;
+
+	return 0;
+}
